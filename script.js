f43f72081d8315b301429760f260e30b0d7692ec
mikhaelesa <56322741+Mikeultron@users.noreply.github.com>
2020-10-15 15:58:48 +0700

    Adding hamburger menu feature

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ let tagline = document.querySelector('.tagline');
 let about = document.querySelector('#about');
 let cases = document.querySelectorAll('.case');
 let links = document.querySelectorAll('a.go');
+let hamburger = document.querySelector('.hamburger');
+let nav = document.querySelector('nav .container')
 
 // Event handler for scrolling.
 window.onscroll = function(){
@@ -14,10 +16,9 @@ window.onscroll = function(){
   tagline.style.opacity = `${1 - (windowScroll / 500)}`;
 
   // Effect for about section
-  if(windowScroll >= about.offsetTop - 500)
-    cases.forEach(c => c.classList.add('show'));
-  else
-    cases.forEach(c => c.classList.remove('show'));
+  cases.forEach(c =>{
+    (windowScroll >= about.offsetTop -500)? c.classList.add('show') : c.classList.remove('show');
+  })
 }
 
 // Smooth scrolling effect
@@ -25,9 +26,15 @@ links.forEach(link =>{
   link.addEventListener('click', function(e){
     e.preventDefault();
     let targetAttr = link.getAttribute('href');
+    if(nav.classList.contains('slide'))
+      nav.classList.remove('slide')
     let targetSection = document.querySelector(targetAttr);
     targetSection.scrollIntoView({
       behavior: "smooth"
     })
   })
+})
+
+hamburger.addEventListener('click', function(){
+  nav.classList.toggle('slide');
 })
